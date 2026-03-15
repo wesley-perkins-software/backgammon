ec2d88cb5c2f791210dc574ac25922403601bd85
Wesley Perkins <wesleyperkins.software@gmail.com>
2026-03-15 16:41:48 -0400

    Complete GA4 setup for Vite SPA route tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import AppRouter from './router/AppRouter.jsx';
+import { installGaPageTracking } from './analytics.js';
 import './styles.css';
+
+
+function AnalyticsTracker() {
+  useEffect(() => installGaPageTracking(), []);
+  return null;
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
+      <AnalyticsTracker />
       <AppRouter />
     </HelmetProvider>
   </React.StrictMode>
